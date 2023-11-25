ae226b1422eefbd82a4356209655b562110701c1
Suoqin Jin <32399212+sqjin@users.noreply.github.com>
2023-11-25 09:39:40 +0800

    Allow to identify over-expressed genes without performing DE
    Update identifyOverExpressedGenes to enable the identification of over-expressed genes without performing DE

--- R/utilities.R
+++ R/utilities.R
@@ -282,7 +282,9 @@ subsetData <- function(object, features = NULL) {
 #' @param thresh.pc Threshold of the percent of cells expressed in one cluster
 #' @param thresh.fc Threshold of Log Fold Change
 #' @param thresh.p Threshold of p-values
+#' @param do.DE Whether perform differential expression analysis. By default do.DE = TRUE; When do.DE = FALSE, selecting over-expressed genes that are expressed in more than `min.cells` cells.
 #' @param do.fast If do.fast = TRUE, then perform a ultra-fast Wilcoxon test using presto package; otherwise using stats package. These two methods produce different logFC values, and the presto::wilcoxauc method gives smaller values.
+#' @param min.cells the minmum number of expressed cells required for the genes that are considered for cell-cell communication analysis
 #' @importFrom future nbrOfWorkers
 #' @importFrom pbapply pbsapply
 #' @importFrom future.apply future_sapply
@@ -295,7 +297,7 @@ subsetData <- function(object, features = NULL) {
 #' @export
 #'
 identifyOverExpressedGenes <- function(object, data.use = NULL, group.by = NULL, idents.use = NULL, invert = FALSE, group.dataset = NULL, pos.dataset = NULL, features.name = "features",  only.pos = TRUE, features = NULL, return.object = TRUE,
-                                       thresh.pc = 0, thresh.fc = 0, thresh.p = 0.05, do.fast = TRUE) {
+                                       thresh.pc = 0, thresh.fc = 0, thresh.p = 0.05, do.DE = TRUE, do.fast = TRUE, min.cells = 10) {
   if (!is.list(object@var.features)) {
     stop("Please update your CellChat object via `updateCellChat()`")
   }
@@ -312,185 +314,198 @@ identifyOverExpressedGenes <- function(object, data.use = NULL, group.by = NULL,
     features.use <- intersect(features, row.names(X))
   }
   data.use <- X[features.use,]
-  data.use <- as.matrix(data.use)
 
-  if (is.null(group.by)) {
-    labels <- object@idents
+  if (do.DE) {
+    # select genes based on differential expression
+    data.use <- as.matrix(data.use)
+
+    if (is.null(group.by)) {
+      labels <- object@idents
+      if (!is.factor(labels)) {
+        message("Use the joint cell labels from the merged CellChat object")
+        labels <- object@idents$joint
+      }
+    } else {
+      labels <- object@meta[[group.by]]
+    }
     if (!is.factor(labels)) {
-      message("Use the joint cell labels from the merged CellChat object")
-      labels <- object@idents$joint
+      labels <- factor(labels)
     }
-  } else {
-    labels <- object@meta[[group.by]]
-  }
-  if (!is.factor(labels)) {
-    labels <- factor(labels)
-  }
-  level.use <- levels(labels)[levels(labels) %in% unique(labels)]
-  if (!is.null(idents.use)) {
-    if (invert) {
-      level.use <- level.use[!(level.use %in% idents.use)]
-    } else {
-      level.use <- level.use[level.use %in% idents.use]
-    }
-  }
-  numCluster <- length(level.use)
-
-  if (!is.null(group.dataset)) {
-    labels.dataset <- as.character(object@meta[[group.dataset]])
-    if (!(pos.dataset %in% unique(labels.dataset))) {
-      cat("Please set pos.dataset to be one of the following dataset names: ", unique(as.character(labels.dataset)))
-      stop()
-    }
-    labels.dataset[labels.dataset != pos.dataset] <- toString(setdiff(unique(labels.dataset), pos.dataset))
-    labels.dataset <- factor(labels.dataset, levels = c(pos.dataset, setdiff(unique(labels.dataset), pos.dataset)))
-  }
-
-  if (do.fast) {
-    if (is.null(group.dataset)) {
-      genes.de <- presto::wilcoxauc(data.use, labels, groups_use = level.use)
-      colnames(genes.de) <- plyr::mapvalues(colnames(genes.de),from = c("group","feature","pval","logFC","pct_in","pct_out","padj"), to = c("clusters","features","pvalues","logFC","pct.1", "pct.2","pvalues.adj"), warn_missing = TRUE)
-      genes.de$logFC_abs <- abs(genes.de$logFC)
-      pct.max <- apply(genes.de[, c("pct.1", "pct.2"), drop = FALSE], MARGIN = 1, FUN = max)
-      genes.de$pct.max <- pct.max
-      markers.all <- dplyr::filter(genes.de, pvalues < thresh.p, logFC_abs >= thresh.fc, pct.max > thresh.pc*100) %>% arrange(pvalues)
-
-    } else {
-      genes.de <- data.frame()
-      for (i in 1:numCluster) {
-        idx <- which(labels == level.use[i])
-        data.use.i <- data.use[ ,idx]
-        labels.i <- labels.dataset[idx]
-        genes.de.i <- presto::wilcoxauc(data.use.i, labels.i)
-        genes.de.i$clusters <- level.use[i]
-        genes.de <- rbind(genes.de, genes.de.i)
+    level.use <- levels(labels)[levels(labels) %in% unique(labels)]
+    if (!is.null(idents.use)) {
+      if (invert) {
+        level.use <- level.use[!(level.use %in% idents.use)]
+      } else {
+        level.use <- level.use[level.use %in% idents.use]
       }
-      colnames(genes.de) <- plyr::mapvalues(colnames(genes.de),from = c("group","feature","pval","logFC","pct_in","pct_out","padj"), to = c("datasets","features","pvalues","logFC","pct.1", "pct.2","pvalues.adj"), warn_missing = TRUE)
-      genes.de$logFC_abs <- abs(genes.de$logFC)
-      pct.max <- apply(genes.de[, c("pct.1", "pct.2"), drop = FALSE], MARGIN = 1, FUN = max)
-      genes.de$pct.max <- pct.max
-      markers.all <- dplyr::filter(genes.de, pvalues < thresh.p, logFC_abs >= thresh.fc, pct.max > thresh.pc*100)
-      markers.all$datasets <- factor(markers.all$datasets, levels = levels(labels.dataset))
-      markers.all <- markers.all[order(markers.all$datasets, markers.all$pvalues, -markers.all$logFC), ]
     }
-    markers.all <- dplyr::select(markers.all, -c("logFC_abs","statistic","pct.max"))
+    numCluster <- length(level.use)
 
-    if (only.pos & nrow(markers.all) > 0) {
-      markers.all <- subset(markers.all, subset = logFC > 0)
+    if (!is.null(group.dataset)) {
+      labels.dataset <- as.character(object@meta[[group.dataset]])
+      if (!(pos.dataset %in% unique(labels.dataset))) {
+        cat("Please set pos.dataset to be one of the following dataset names: ", unique(as.character(labels.dataset)))
+        stop()
+      }
+      labels.dataset[labels.dataset != pos.dataset] <- toString(setdiff(unique(labels.dataset), pos.dataset))
+      labels.dataset <- factor(labels.dataset, levels = c(pos.dataset, setdiff(unique(labels.dataset), pos.dataset)))
     }
-    markers.all$features <- as.character(markers.all$features)
 
-    features.sig <- markers.all$features
-    object@var.features[[features.name]] <- unique(features.sig)
-    features.name <- paste0(features.name, ".info")
-    object@var.features[[features.name]] <- markers.all
-
-  } else {
-
-    my.sapply <- ifelse(
-      test = future::nbrOfWorkers() == 1,
-      yes = pbapply::pbsapply,
-      no = future.apply::future_sapply
-    )
-
-    mean.fxn <- function(x) {
-      return(log(x = mean(x = expm1(x = x)) + 1))
-    }
-    labels <- as.character(labels)
-    genes.de <- vector("list", length = numCluster)
-    for (i in 1:numCluster) {
-      features <- features.use
+    if (do.fast) {
       if (is.null(group.dataset)) {
-        cell.use1 <- which(labels == level.use[i])
-        cell.use2 <- base::setdiff(1:length(labels), cell.use1)
+        genes.de <- presto::wilcoxauc(data.use, labels, groups_use = level.use)
+        colnames(genes.de) <- plyr::mapvalues(colnames(genes.de),from = c("group","feature","pval","logFC","pct_in","pct_out","padj"), to = c("clusters","features","pvalues","logFC","pct.1", "pct.2","pvalues.adj"), warn_missing = TRUE)
+        genes.de$logFC_abs <- abs(genes.de$logFC)
+        pct.max <- apply(genes.de[, c("pct.1", "pct.2"), drop = FALSE], MARGIN = 1, FUN = max)
+        genes.de$pct.max <- pct.max
+        markers.all <- dplyr::filter(genes.de, pvalues < thresh.p, logFC_abs >= thresh.fc, pct.max > thresh.pc*100) %>% arrange(pvalues)
+
       } else {
-        cell.use1 <- which((labels == level.use[i]) & (labels.dataset == pos.dataset))
-        cell.use2 <- which((labels == level.use[i]) & (labels.dataset != pos.dataset))
+        genes.de <- data.frame()
+        for (i in 1:numCluster) {
+          idx <- which(labels == level.use[i])
+          data.use.i <- data.use[ ,idx]
+          labels.i <- labels.dataset[idx]
+          genes.de.i <- presto::wilcoxauc(data.use.i, labels.i)
+          genes.de.i$clusters <- level.use[i]
+          genes.de <- rbind(genes.de, genes.de.i)
+        }
+        colnames(genes.de) <- plyr::mapvalues(colnames(genes.de),from = c("group","feature","pval","logFC","pct_in","pct_out","padj"), to = c("datasets","features","pvalues","logFC","pct.1", "pct.2","pvalues.adj"), warn_missing = TRUE)
+        genes.de$logFC_abs <- abs(genes.de$logFC)
+        pct.max <- apply(genes.de[, c("pct.1", "pct.2"), drop = FALSE], MARGIN = 1, FUN = max)
+        genes.de$pct.max <- pct.max
+        markers.all <- dplyr::filter(genes.de, pvalues < thresh.p, logFC_abs >= thresh.fc, pct.max > thresh.pc*100)
+        markers.all$datasets <- factor(markers.all$datasets, levels = levels(labels.dataset))
+        markers.all <- markers.all[order(markers.all$datasets, markers.all$pvalues, -markers.all$logFC), ]
       }
+      markers.all <- dplyr::select(markers.all, -c("logFC_abs","statistic","pct.max"))
 
-      # feature selection (based on percentages)
-      thresh.min <- 0
-      pct.1 <- round(
-        x = rowSums(data.use[features, cell.use1, drop = FALSE] > thresh.min) /
-          length(x = cell.use1),
-        digits = 3
+      if (only.pos & nrow(markers.all) > 0) {
+        markers.all <- subset(markers.all, subset = logFC > 0)
+      }
+      markers.all$features <- as.character(markers.all$features)
+
+      features.sig <- markers.all$features
+      object@var.features[[features.name]] <- unique(features.sig)
+      features.name <- paste0(features.name, ".info")
+      object@var.features[[features.name]] <- markers.all
+
+    } else {
+
+      my.sapply <- ifelse(
+        test = future::nbrOfWorkers() == 1,
+        yes = pbapply::pbsapply,
+        no = future.apply::future_sapply
       )
-      pct.2 <- round(
-        x = rowSums(data.use[features, cell.use2, drop = FALSE] > thresh.min) /
-          length(x = cell.use2),
-        digits = 3
-      )
-      data.alpha <- cbind(pct.1, pct.2)
-      colnames(x = data.alpha) <- c("pct.1", "pct.2")
-      alpha.min <- apply(X = data.alpha, MARGIN = 1, FUN = max)
-      names(x = alpha.min) <- rownames(x = data.alpha)
-      features <- names(x = which(x = alpha.min > thresh.pc))
-      if (length(x = features) == 0) {
-        #stop("No features pass thresh.pc threshold")
-        next
+
+      mean.fxn <- function(x) {
+        return(log(x = mean(x = expm1(x = x)) + 1))
       }
+      labels <- as.character(labels)
+      genes.de <- vector("list", length = numCluster)
+      for (i in 1:numCluster) {
+        features <- features.use
+        if (is.null(group.dataset)) {
+          cell.use1 <- which(labels == level.use[i])
+          cell.use2 <- base::setdiff(1:length(labels), cell.use1)
+        } else {
+          cell.use1 <- which((labels == level.use[i]) & (labels.dataset == pos.dataset))
+          cell.use2 <- which((labels == level.use[i]) & (labels.dataset != pos.dataset))
+        }
 
-      # feature selection (based on average difference)
-      data.1 <- apply(X = data.use[features, cell.use1, drop = FALSE],MARGIN = 1,FUN = mean.fxn)
-      data.2 <- apply(X = data.use[features, cell.use2, drop = FALSE],MARGIN = 1,FUN = mean.fxn)
-      FC <- (data.1 - data.2)
-      if (only.pos) {
-        features.diff <- names(which(FC > thresh.fc))
-      } else {
-        features.diff <- names(which(abs(FC) > thresh.fc))
-      }
-
-      features <- intersect(x = features, y = features.diff)
-      if (length(x = features) == 0) {
-        #  stop("No features pass thresh.fc threshold")
-        next
-      }
-
-      data1 <- data.use[features, cell.use1, drop = FALSE]
-      data2 <- data.use[features, cell.use2, drop = FALSE]
-
-      pvalues <- unlist(
-        x = my.sapply(
-          X = 1:nrow(x = data1),
-          FUN = function(x) {
-            # return(wilcox.test(data1[x, ], data2[x, ], alternative = "greater")$p.value)
-            return(wilcox.test(data1[x, ], data2[x, ])$p.value)
-          }
+        # feature selection (based on percentages)
+        thresh.min <- 0
+        pct.1 <- round(
+          x = rowSums(data.use[features, cell.use1, drop = FALSE] > thresh.min) /
+            length(x = cell.use1),
+          digits = 3
         )
-      )
+        pct.2 <- round(
+          x = rowSums(data.use[features, cell.use2, drop = FALSE] > thresh.min) /
+            length(x = cell.use2),
+          digits = 3
+        )
+        data.alpha <- cbind(pct.1, pct.2)
+        colnames(x = data.alpha) <- c("pct.1", "pct.2")
+        alpha.min <- apply(X = data.alpha, MARGIN = 1, FUN = max)
+        names(x = alpha.min) <- rownames(x = data.alpha)
+        features <- names(x = which(x = alpha.min > thresh.pc))
+        if (length(x = features) == 0) {
+          #stop("No features pass thresh.pc threshold")
+          next
+        }
 
-      pval.adj = stats::p.adjust(
-        p = pvalues,
-        method = "bonferroni",
-        n = nrow(X)
-      )
-      genes.de[[i]] <- data.frame(clusters = level.use[i], features = as.character(rownames(data1)), pvalues = pvalues, logFC = FC[features], data.alpha[features,, drop = F],pvalues.adj = pval.adj, stringsAsFactors = FALSE)
-    }
+        # feature selection (based on average difference)
+        data.1 <- apply(X = data.use[features, cell.use1, drop = FALSE],MARGIN = 1,FUN = mean.fxn)
+        data.2 <- apply(X = data.use[features, cell.use2, drop = FALSE],MARGIN = 1,FUN = mean.fxn)
+        FC <- (data.1 - data.2)
+        if (only.pos) {
+          features.diff <- names(which(FC > thresh.fc))
+        } else {
+          features.diff <- names(which(abs(FC) > thresh.fc))
+        }
 
-    markers.all <- data.frame()
-    for (i in 1:numCluster) {
-      gde <- genes.de[[i]]
-      if (!is.null(gde)) {
-        gde <- gde[order(gde$pvalues, -gde$logFC), ]
-        gde <- subset(gde, subset = pvalues < thresh.p)
-        if (nrow(gde) > 0) {
-          markers.all <- rbind(markers.all, gde)
+        features <- intersect(x = features, y = features.diff)
+        if (length(x = features) == 0) {
+          #  stop("No features pass thresh.fc threshold")
+          next
+        }
+
+        data1 <- data.use[features, cell.use1, drop = FALSE]
+        data2 <- data.use[features, cell.use2, drop = FALSE]
+
+        pvalues <- unlist(
+          x = my.sapply(
+            X = 1:nrow(x = data1),
+            FUN = function(x) {
+              # return(wilcox.test(data1[x, ], data2[x, ], alternative = "greater")$p.value)
+              return(wilcox.test(data1[x, ], data2[x, ])$p.value)
+            }
+          )
+        )
+
+        pval.adj = stats::p.adjust(
+          p = pvalues,
+          method = "bonferroni",
+          n = nrow(X)
+        )
+        genes.de[[i]] <- data.frame(clusters = level.use[i], features = as.character(rownames(data1)), pvalues = pvalues, logFC = FC[features], data.alpha[features,, drop = F],pvalues.adj = pval.adj, stringsAsFactors = FALSE)
+      }
+
+      markers.all <- data.frame()
+      for (i in 1:numCluster) {
+        gde <- genes.de[[i]]
+        if (!is.null(gde)) {
+          gde <- gde[order(gde$pvalues, -gde$logFC), ]
+          gde <- subset(gde, subset = pvalues < thresh.p)
+          if (nrow(gde) > 0) {
+            markers.all <- rbind(markers.all, gde)
+          }
         }
       }
-    }
-    if (only.pos & nrow(markers.all) > 0) {
-      markers.all <- subset(markers.all, subset = logFC > 0)
-    }
-    if (!is.null(group.dataset)) {
-      markers.all$datasets[markers.all$logFC > 0] <- pos.dataset
-      markers.all$datasets[markers.all$logFC < 0] <- setdiff(unique(labels.dataset), pos.dataset)
-      markers.all$datasets <- factor(markers.all$datasets, levels = levels(labels.dataset))
-      markers.all <- markers.all[order(markers.all$datasets, markers.all$pvalues, -markers.all$logFC), ]
-    }
-    markers.all$features <- as.character(markers.all$features)
+      if (only.pos & nrow(markers.all) > 0) {
+        markers.all <- subset(markers.all, subset = logFC > 0)
+      }
+      if (!is.null(group.dataset)) {
+        markers.all$datasets[markers.all$logFC > 0] <- pos.dataset
+        markers.all$datasets[markers.all$logFC < 0] <- setdiff(unique(labels.dataset), pos.dataset)
+        markers.all$datasets <- factor(markers.all$datasets, levels = levels(labels.dataset))
+        markers.all <- markers.all[order(markers.all$datasets, markers.all$pvalues, -markers.all$logFC), ]
+      }
+      markers.all$features <- as.character(markers.all$features)
 
+      features.sig <- markers.all$features
+      object@var.features[[features.name]] <- features.sig
+      features.name <- paste0(features.name, ".info")
+      object@var.features[[features.name]] <- markers.all
+    }
+
+  } else {
+    # select genes if they are exprssed in at least `min.cells` cells
+    markers.all <- data.frame(features = as.character(rownames(data.use)), nCells = rowSums(data.use > 0))
+    markers.all <- dplyr::filter(markers.all, nCells >= min.cells)
     features.sig <- markers.all$features
-    object@var.features[[features.name]] <- features.sig
+    object@var.features[[features.name]] <- unique(features.sig)
     features.name <- paste0(features.name, ".info")
     object@var.features[[features.name]] <- markers.all
   }
