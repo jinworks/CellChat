e003aa163abe4bee39528b98e29e5b34ce54001d
Suoqin Jin <32399212+sqjin@users.noreply.github.com>
2023-11-25 10:12:27 +0800

    Update subsetDB

--- R/database.R
+++ R/database.R
@@ -125,7 +125,7 @@ searchPair <- function(signaling = c(), pairLR.use, key = c("pathway_name","liga
 #' Subset CellChatDB databse by only including interactions of interest
 #'
 #' @param CellChatDB CellChatDB databse
-#' @param search a character vector; When `key` is a vector, the `search` should be a list with the size being `length(key)`, where each element is a character vector
+#' @param search a character vector, which is a subset of c("Secreted Signaling","ECM-Receptor","Cell-Cell Contact","Non-protein Signaling"); When `key` is a vector, the `search` should be a list with the size being `length(key)`, where each element is a character vector.
 #' @param key a character vector and each element should be one of the column names of the interaction_input from CellChatDB.
 #' @param non_protein whether to use the non-protein signaling for CellChat analysis. By default, non_protein = FALSE because most of non-protein signaling are the special synaptic signaling interactions that can only be used when inferring neuron-neuron communication.
 #'
@@ -134,6 +134,10 @@ searchPair <- function(signaling = c(), pairLR.use, key = c("pathway_name","liga
 #'
 subsetDB <- function(CellChatDB, search = c(), key = "annotation", non_protein = FALSE) {
   interaction_input <- CellChatDB$interaction
+  if ("Non-protein Signaling" %in% unlist(search)) {
+    non_protein = TRUE
+    message("The non-protein signaling is now included for CellChat analysis, which is usually used for neuron-neuron communication!")
+  }
   if (non_protein == FALSE) {
     interaction_input <- subset(interaction_input, annotation != "Non-protein Signaling")
   }
